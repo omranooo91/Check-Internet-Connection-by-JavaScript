1ede35f94746eaab4c712f13469a590b0720bc0b
Omran Hallak <omran.hallak@gmail.com>
2022-09-07 21:39:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let title = document.querySelector('.title');
+let ul = document.querySelector('ul');
+let reload = document.querySelector('.reload');
+
+
+window.onload = function(){
+    if(window.navigator.onLine){
+        online();
+    }else{
+        offline();
+    }
+}
+
+window.addEventListener("online", function(){
+    online();
+});
+
+window.addEventListener("offline", function(){
+    offline();
+});
+
+
+function online() {
+    title.innerHTML = 'Online now';
+    title.style.color = 'green';
+    ul.classList.add('hide');
+    reload.classList.add('hide');
+
+}
+
+
+function offline() {
+    title.innerHTML = 'offline now';
+    title.style.color = '#666';
+    ul.classList.remove('hide');
+    reload.classList.remove('hide');
+
+}
